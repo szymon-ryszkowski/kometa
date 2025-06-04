67ffb0ef669e6b2e48c80ddcbc13c96727788280
Dawid <dawidchudzik07@gmail.com>
2025-06-04 23:04:57 +0200

    dodanie momentu pędu

--- celestial body.py
+++ celestial body.py
@@ -2,7 +2,7 @@ from math import *
 '''
 oto klasa która będzie reprezentować Ziemię i kometę
 Deklaruje je się tak jak poniżej:
-Ziemia = celestial_body(a_z, e_z, i_z, t_0_z, arg_of_per_z, long_of_asc_z, r_z, t_z, M_s)
+Ziemia = celestial_body(a_z, e_z, i_z, t_0_z, arg_of_per_z, long_of_asc_z, r_z, t_z, 1.9891*10**30, 6.6743*10**-11)
 Gdzie:
 a - półoś wielka[m]
 e - mimośród[bez jednostek]
@@ -20,7 +20,7 @@ Program oblicza współrzędne x, y, z oraz składowe v_x, v_Y, v_z
 do konwersji współrzędnych potrzebny jest jeszcze czas t[s] w momencie początka symulacji(UWAGA, nie jest
 on używany do obliczania czegokolwiek bezpośrednio, podobnie t_0. Jest to informacja dla programu, że gdy
  t < t_0 to ciało się zbliża do Słońca, a gdy t > t_0, to ciało się oddala) oraz początkowa odległość od Słońca r[m]. Potrzebna
- jest także masa ciała centralnego M. 
+ jest także masa ciała centralnego M i stała grawitacji G. 
 
 Wykorzytuję układ współrzędnych kartezjańskich, gdzie oś x leci w kierunku punktu barana, z jest prostopadłe do ekliptyki, a y
 jest prostopadłe do x i z i jednocześnie ukłąd pozostaje prawoskrętny(tzn. od osi x do osi y obracamy się przeciwnie do wskazówek zegara)
@@ -29,12 +29,12 @@ jest prostopadłe do x i z i jednocześnie ukłąd pozostaje prawoskrętny(tzn. 
 Pomocniczo wykorzystuję sferyczny układ współrzędnych. Kąt phi jest u mnie w zakresie -90 <= phi <= 90
 theta = 0 jest dla kierunku dodatnich X-ów. 
 
-
+dodatkowo obliczany jest moment pędu L
 '''
 
 
 class celestial_body:
-    def __init__(self, a, e, i, t_0, arg_of_per, long_of_asc, r, t, M):
+    def __init__(self, a, e, i, t_0, arg_of_per, long_of_asc, r, t, M, G):
         self.a = a
         self.e = e
         self.i = radians(i)
@@ -50,6 +50,8 @@ class celestial_body:
         self.v_y = 0
         self.v_z = 0
         self.M = M
+        self.G = G
+        self.L = sqrt(self.G*self.M * self.a * (1 - self.e**2))
         self.convert_cartesian_coordinates()
 
     def convert_cartesian_coordinates(self):
