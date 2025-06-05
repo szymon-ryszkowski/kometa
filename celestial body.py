51d3d06b3dce48697082d8164631c23aad07bf71
Dawid <dawidchudzik07@gmail.com>
2025-06-05 17:11:19 +0200

    dodanie konwersji prędkości

--- celestial body.py
+++ celestial body.py
@@ -55,6 +55,7 @@ class celestial_body:
         self.convert_cartesian_coordinates()
 
     def convert_cartesian_coordinates(self):
+        #pomocnicze kąty
         cos_theta = ((self.a*(1 - self.e**2)/self.r) - 1)/self.e
         theta = 0
         if(self.t > self.t_0):
@@ -62,15 +63,53 @@ class celestial_body:
         else:
             theta = 2*pi - acos(cos_theta)
         angle_1 = self.arg_of_per+theta
-        self.z = sin(self.i) * sin(angle_1) * self.r
-        cos_vertical = sqrt(1 - (sin(self.i) * sin(self.arg_of_per + theta))**2)
+        print(angle_1)
+        sin_vertical = sin(self.i) * sin(angle_1)
+        cos_vertical = sqrt(1 - (sin(self.i) * sin(angle_1))**2)
         if(self.i > pi/2):
             cos_vertical *= -1
-        cos_horizontal = cos(angle_1)/cos_vertical
-        sin_horizontal = sqrt(1 - cos_horizontal**2)
+        cos_horizontal_pom = cos(angle_1)/cos_vertical
+        sin_horizontal_pom = sqrt(1 - cos_horizontal_pom**2)
         if angle_1 > pi:
-            sin_horizontal *= -1
+            sin_horizontal_pom *= -1
+        cos_horizontal = cos_horizontal_pom * cos(self.long_of_asc) - sin_horizontal_pom*sin(self.long_of_asc)
+        sin_horizontal = sin_horizontal_pom*cos(self.long_of_asc) + cos_horizontal_pom*sin(self.long_of_asc)
+
+        #położenie
         self.x = self.r*cos_vertical * cos_horizontal
         self.y = self.r*cos_vertical * sin_horizontal
-    def calculate_Velocity(self):
-        self.v_x = self.v_x
+        self.z = sin(self.i) * sin(angle_1) * self.r
+
+        #prędkosci
+        v = sqrt(self.G * self.M * (2 / self.r - 1 / self.a))
+        v_tan = self.L / self.r
+        v_rad = sqrt(v**2 - v_tan**2)
+        if self.t > self.t_0:
+            v_rad *= -1
+        #prędkość radialna
+        self.v_z += v_rad*sin_vertical
+        self.v_x -= v_rad*cos_vertical*cos_horizontal
+        self.v_y -= v_rad*cos_vertical*sin_horizontal
+        #predkość tangencjalna
+        #rozbicie wzdłuż równoleżnika i południka
+        sin_beta = 0
+        if sin(angle_1) != 0:
+            sin_beta = sin_horizontal_pom / sin(angle_1)
+        else:
+            sin_beta = sin(self.i)
+        v_latitude = v_tan*sin_beta
+        cos_beta = sqrt(1 - sin_beta**2)
+        if(cos_horizontal_pom < 0):
+            cos_beta *= -1
+        v_longitude = v_tan*cos_beta
+        #prędkość wzdłuż południków
+        self.v_z += v_longitude*cos_vertical
+        self.v_x -= v_longitude*sin_vertical*cos_horizontal
+        self.v_y -= v_longitude*sin_vertical*sin_horizontal
+        #prędkość wzdłuż równoleżników
+        self.v_x -= v_latitude*sin_horizontal
+        self.v_y += v_latitude*cos_horizontal
+
+#Ziemia = celestial_body(1.496*10**11, 0.0167, 45, -1, 90, 90, 1.496*10**11, 0, 1.9891*10**30, 6.6743*10**-11)
+#print(Ziemia.x, Ziemia.y, Ziemia.z)
+#print(Ziemia.v_x, Ziemia.v_y, Ziemia.v_z)
