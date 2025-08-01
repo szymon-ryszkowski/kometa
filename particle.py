5662f727f05f6099a3bb3e8f18eaeadbb58edb48
Dawid <dawidchudzik07@gmail.com>
2025-08-01 19:43:59 +0200

    powstanie pliku i generacja wektora prędkości. Dodano funkcię create_particle

--- particle.py
+++ particle.py
@@ -14,3 +14,14 @@ def generate_velocity(v):
         velocity[1] = v*cos_theta*sin(angle) # składowa y
         velocity[0] = v*cos_theta*cos(angle) #składowa x
     return velocity
+def create_particle(v, x, y, z):
+    velocity = generate_velocity(v)
+    particle = np.zeros(1, 7)
+    particle[0] = "H_2O"
+    particle[1] = x # x
+    particle[2] = y # y
+    particle[3] = z # z
+    particle[4] = velocity[0] # v_x
+    particle[5] = velocity[1] # v_y
+    particle[6] = velocity[2] # v_z
+    return particle
