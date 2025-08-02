f07958b8e971ac285645b9ce1a80c33e4c448fab
Dawid <dawidchudzik07@gmail.com>
2025-08-02 21:52:10 +0200

    dodano wizualizację trajektorii komety

--- loop.py
+++ loop.py
@@ -1,10 +1,19 @@
 import config
 import particle as pt
 import celestial_body as cb
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 kometa = cb.celestial_body(2*config.AU, 0.2, 0, 0, 90, 90, 2*config.AU, -1, config.M, config.G)
-
+#zapis trajektorii komety
+x_traj = []
+y_traj = []
+z_traj = []
+print(kometa.z)
 for i in range(config.n_steps):
+    x_traj.append(kometa.x)
+    y_traj.append(kometa.y)
+    z_traj.append(kometa.z)
     #utwórz cząstki
     ratio = pt.calculate_sim_ratio(config.absolute_ratio_H_2O, kometa.r, 2)
     pt.queue_H_2O += ratio*config.dt
@@ -17,6 +26,18 @@ for i in range(config.n_steps):
     acceleration_x = -kometa.x*config.G*config.M/(kometa.x**2 + kometa.y**2 + kometa.z**2)**1.5
     acceleration_y = -kometa.y*config.G*config.M/(kometa.x**2 + kometa.y**2 + kometa.z**2)**1.5
     acceleration_z = -kometa.z*config.G*config.M/(kometa.x**2 + kometa.y**2 + kometa.z**2)**1.5
+    print(acceleration_x, acceleration_y, acceleration_z)
     kometa.v_x += acceleration_x*config.dt
     kometa.v_y += acceleration_y*config.dt
     kometa.v_z += acceleration_z*config.dt
+#wizualizacja 3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x_traj, y_traj, z_traj, label='Trajektoria komety')
+ax.scatter(0, 0, 0, color='yellow', label='Słońce')  # Pozycja Słońca w środku
+ax.set_xlabel('X [m]')
+ax.set_ylabel('Y [m]')
+ax.set_zlabel('Z [m]')
+ax.set_title('Trajektoria komety w 3D')
+ax.legend()
+plt.show()
